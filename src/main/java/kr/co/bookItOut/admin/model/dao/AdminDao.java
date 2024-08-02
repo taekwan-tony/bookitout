ed69bfbd2d55412ca0955229158d32498ef3a077
taekwan-tony <gtkwan1@naver.com>
2024-08-02 09:40:55 +0900

    기초세팅 수정

--- src/main/java/kr/co/bookItOut/admin/model/dao/AdminDao.java
+++ src/main/java/kr/co/bookItOut/admin/model/dao/AdminDao.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import kr.co.bookItOut.admin.model.dto.AdminRowMapper;
+
 @Repository
 public class AdminDao {
 	@Autowired
 	private JdbcTemplate jdbc;
 	@Autowired
-	private AdminDao adminDao;
+	private AdminRowMapper adminRowMapper;
 	
 	
 }
