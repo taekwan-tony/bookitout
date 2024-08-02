06ceddfe5c83e4dff7b797c332c88b9429dc681b
KingyeonU <kattack0702@naver.com>
2024-08-02 14:37:57 +0900

    마이페이지 파일 추가

---
+++ src/main/java/kr/co/bookItOut/Mypage/controller/MypageController.java
@@ -0,0 +1,16 @@
+package kr.co.bookItOut.Mypage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/myPage")
+public class MypageController {
+	@GetMapping(value = "/myPage")
+	private String myPage() {
+		
+		return "myPage/myPage";
+	}
+	
+}
