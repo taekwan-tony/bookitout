34f173938db64f6897e34733b93e966c5eecfa33
KingyeonU <kattack0702@naver.com>
2024-08-02 18:04:54 +0900

    마이페이지 , 로그인 페이지
    마이페이지 - 내 정보, 주문 조회 화면 수정,
    
    로그인 페이지 파일 추가

--- src/main/java/kr/co/bookItOut/member/controller/MemberController.java
+++ src/main/java/kr/co/bookItOut/member/controller/MemberController.java
@@ -2,25 +2,54 @@ package kr.co.bookItOut.member.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpSession;
+import kr.co.bookItOut.member.model.dto.Member;
 import kr.co.bookItOut.member.model.service.MemberService;
 
 @Controller
-@RequestMapping(value="/member")
+@RequestMapping(value = "/member")
 public class MemberController {
 
 	@Autowired
 	private MemberService memberService;
-	
+
 	@GetMapping(value = "/mypage")
 	private String myPage() {
 		return "myPage/myPage";
 	}
-	
+
 	@GetMapping(value = "/myInfo")
 	private String myInfo() {
 		return "myPage/myInfo";
 	}
+
+	@GetMapping(value = "/myOrder")
+	private String myOrder() {
+		return "myPage/myOrder";
+	}
+
+	
+	@GetMapping(value="/loginFrm")
+	public String loginFrm() {
+		return "member/login";
+	}
+	
+//	@PostMapping(value = "/login")
+//	public String login(Member m, Model model, HttpSession session) {
+//		// service를 이용해서 DB에 입력받은 아이디/패스워드가 일치하는 회원 조회
+//		// 조회조건에 아이디가 포함되어있으므로 조회결과는 회원1명 또는 0명 -> Member타입으로 결과 받기
+//		Member member = memberService.selectOneMember(m);
+//		if (member == null) {
+//			
+//			return "member/logIn";
+//		} else {
+//			session.setAttribute("member", member);
+//			return "redirect:/";
+//		}
+//	}
 }
