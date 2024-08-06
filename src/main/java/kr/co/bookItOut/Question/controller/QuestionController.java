41dbd97b8f28b069a11b2f42ee110ffcc8dcc691
taekwan-tony <gtkwan1@naver.com>
2024-08-06 09:27:29 +0900

    1:1문의 초기 업로드

--- src/main/java/kr/co/bookItOut/Question/controller/QuestionController.java
+++ src/main/java/kr/co/bookItOut/Question/controller/QuestionController.java
@@ -3,8 +3,11 @@ package kr.co.bookItOut.Question.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import kr.co.bookItOut.Question.model.dto.Question;
 import kr.co.bookItOut.Question.model.service.QuestionService;
 import kr.co.bookItOut.util.FileUtils;
 
@@ -18,6 +21,9 @@ public class QuestionController {
 	@Value("${file.root}")
 	private String root;
 	
-	
+	@PostMapping(value="/insertQuestion")
+	public String insertQuestion(Question q,Model model) {
+		return null;
+	}
 	
 }
