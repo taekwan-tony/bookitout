5188bd5af5a8e53ead6c1debc8014691ab2cd34d
taekwan-tony <gtkwan1@naver.com>
2024-08-08 17:22:03 +0900

    1:1문의 답변처리 구현중

--- src/main/java/kr/co/bookItOut/admin/controller/AdminController.java
+++ src/main/java/kr/co/bookItOut/admin/controller/AdminController.java
@@ -1,5 +1,7 @@
 package kr.co.bookItOut.admin.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,7 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import kr.co.bookItOut.Question.model.dto.Question;
+import kr.co.bookItOut.Question.model.dto.QuestionListData;
+import kr.co.bookItOut.Question.model.service.QuestionService;
 import kr.co.bookItOut.admin.model.dto.Admin;
 import kr.co.bookItOut.admin.model.dto.AdminListData;
 import kr.co.bookItOut.admin.model.service.AdminService;
@@ -19,6 +25,12 @@ import kr.co.bookItOut.member.model.dto.Member;
 public class AdminController {
 	@Autowired
 	private AdminService adminService;
+	/**
+	 * 1:1문의 service 어노테이션
+	 * **/
+	@Autowired
+	private QuestionService questionService;
+	
 	//판매점 리스트
 	@GetMapping(value="/adminIndex")
 	public String index(Model model,int rePage) {
@@ -82,6 +94,24 @@ public class AdminController {
 		return "/admin/insertBook";
 	}
 
+	
+//1:1문의 관리자 처리 
+	@GetMapping(value="/questionAnswer")
+	public String questionAnswerList(int reqPage,Model model) {
+		QuestionListData qld = questionService.selectAllQuestion(reqPage);
+		model.addAttribute("list",qld.getList());
+		model.addAttribute("pageNavi",qld.getPageNavi());
+		return "admin/questionAnswer";
+	}
+	
+	@GetMapping(value="/questionAnswerFrm")
+	public String questionAnswerFrm(int questionNo,Model model) {
+		Question q = questionService.selectOneQuestion(questionNo);
+		List fileList = questionService.selectAllFile(questionNo);
+		q.setFileList(fileList);
+		model.addAttribute("q",q);
+		return "admin/questionAnswerFrm";
+	}
 	
 	
 	
