273aca0c8dabae75536068d202f1533afa54eca6
taekwan-tony <gtkwan1@naver.com>
2024-08-09 17:31:49 +0900

    공지사항 - HTML, 작성-상세페이지 작성중

--- src/main/java/kr/co/bookItOut/notice/dao/NoticeDao.java
+++ src/main/java/kr/co/bookItOut/notice/dao/NoticeDao.java
@@ -1,5 +1,7 @@
 package kr.co.bookItOut.notice.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -12,6 +14,19 @@ public class NoticeDao {
 	private JdbcTemplate jdbc;
 	@Autowired
 	private NoticeRowMapper noticeRowMapper;
+	
+	public List selectAllNotice(int start, int end) {
+		String query = "select * from (select rownum as rnum, n.* from (select * from notice order by 1 desc)n) where rnum between ? and ?";
+		Object[] params = {start,end};
+		List list = jdbc.query(query, noticeRowMapper,params);
+		return list;
+	}
+
+	public int totalCount() {
+		String query = "select count(*) from notice";
+		int totalCount = jdbc.queryForObject(query, Integer.class);
+		return totalCount;
+	}
 	
 	
 }
