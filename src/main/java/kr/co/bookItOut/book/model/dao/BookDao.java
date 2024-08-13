c4581bc98e77ef32f107691e1bafa33d312fd67c
subin002003 <leesubin98619@gmail.com>
2024-08-13 17:10:03 +0900

    코드 수정

--- src/main/java/kr/co/bookItOut/book/model/dao/BookDao.java
+++ src/main/java/kr/co/bookItOut/book/model/dao/BookDao.java
@@ -496,10 +496,14 @@ public class BookDao {
 		return likeCount;
 	}
 
-	public List<Book> selectThreeBook(int i) {
-		String query = "select * from (select rownum as rnum, b.* from (select * from book order by 1 desc)b) where rnum between ? and ?";
-		Object[] params = {i,i+3};
-		List list = jdbc.query(query, bookRowMapper,params);
+	public List<Book> selectThreeBook() {
+		String query = "select * from (select rownum as rnum, b.* from (select * from book order by 1 desc)b) where rnum between 1 and 4";
+		List list = jdbc.query(query, bookRowMapper);
+		return list;
+	}
+	public List<Book> selectFiveBook() {
+		String query = "select * from (select rownum as rnum, b.* from (select * from book order by book_genre desc)b) where rnum between 1 and 5";
+		List list = jdbc.query(query, bookRowMapper);
 		return list;
 	}
 	public List<CenterMap> selectOneMap(int adminNo) {
