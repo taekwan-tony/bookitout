d301e42de521c1307a4ffe3b6d0e42aed592805f
Goyoungyun967 <mda208@naver.com>
2024-08-14 13:32:56 +0900

    asd

--- src/main/java/kr/co/bookItOut/admin/model/dao/AdminDao.java
+++ src/main/java/kr/co/bookItOut/admin/model/dao/AdminDao.java
@@ -116,7 +116,7 @@ public class AdminDao {
 	//-책 리스트 끝
 	//-책 등록//
 	public int insertBook(Book book ,Admin admin) {
-		String qurey = "insert into book values(book_seq.nextval,?,?,?,?,?,to_char(sysdate,'yyyy-mm-dd'),?,?,?,?,?,?,?,0)";
+		String qurey = "insert into book values(book_seq.nextval,?,?,?,?,?,to_char(sysdate,'yyyy-mm-dd'),?,?,?,?,?,?,0)";
 //				"insert all " + 
 //				"into admin_tbl(admin_no) values (?);" + 
 //				"into center_inventory(center_book_count,) values(1);" + 
@@ -129,7 +129,7 @@ public class AdminDao {
 							book.getBookName(),book.getBookWriter(),
 							book.getBookPrice(),book.getBookPublisher(),
 							book.getPublicationDate(),book.getBookImg(),
-							admin.getAdminNo(),book.getBookDetailContent(),
+							book.getBookDetailContent(),
 							book.getBookDetailWriter(),book.getBookDetailImg(),
 							book.getBookType(),book.getBookGenre()};
 		int result = jdbc.update(qurey,params);
@@ -207,8 +207,8 @@ public class AdminDao {
 	public int inserOrderAdmin(int centerBookNo,int orderBookCount,Admin admin) {
 		String query = "insert into order_tbl values(order_tbl_seq.nextval,?,to_char(sysdate,'yyyy-mm-dd'),1,?,?)";
 		Object[] params = {orderBookCount,admin.getAdminNo(),centerBookNo};
-		
 		int result = jdbc.update(query,params);
+		System.out.println(result);
 		return result;
 	}
 	public int selectOrderTotoalCount(Admin admin,int type) {
@@ -227,6 +227,7 @@ public class AdminDao {
 				"WHERE admin_tbl.admin_no = ? and order_check=?)o) where rnum between ? and ?";
 		Object[] params = {admin.getAdminNo(),type,start,end};
 		List list = jdbc.query(query,adminOrderBookRowMapper,params);
+		System.out.println(list);
 		return list;
 	}
 	//가맹점 발주버튼 눌렀을때 
